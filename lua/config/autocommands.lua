5348c4722aaacf262f28901c81a86cb39f9dc7b9
Michael Bobbitt <mbobbitt@hntb.com>
2024-12-02 12:57:39 -0500

    added autocmd to autoclose nvim for special buffers

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,13 +1,35 @@
 vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("no_newline_comment", { clear = true }),
-    pattern = "*",
-    command = "setlocal formatoptions-=cro",
-    desc = "Disables automatic commenting on newline",
+	group = vim.api.nvim_create_augroup("no_newline_comment", { clear = true }),
+	pattern = "*",
+	command = "setlocal formatoptions-=cro",
+	desc = "Disables automatic commenting on newline",
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+-- Define a list of buffer types to check
+local special_buffers = { "NeogitStatus", "neo-tree", "TelescopePrompt", "alpha" }
+vim.api.nvim_create_augroup("AutoCloseSpecialBuffers", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = "AutoCloseSpecialBuffers",
+	callback = function()
+		local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+		local all_special = true
+		for _, buf in ipairs(buffers) do
+			local buf_ft = vim.api.nvim_buf_get_option(buf.bufnr, "filetype")
+			if not vim.tbl_contains(special_buffers, buf_ft) then
+				all_special = false
+				break
+			end
+		end
+
+		if all_special then
+			vim.cmd("qa")
+		end
+	end,
 })

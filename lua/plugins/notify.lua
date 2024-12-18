99af15725f29dbeaac0e3714b589b75e8f6d327e
Michael Bobbitt <mbobbitt@hntb.com>
2024-12-18 16:54:17 -0500

    notify defaults

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,7 +2,25 @@ return {
 	"rcarriga/nvim-notify",
 	config = function()
 		require("notify").setup({
-			background_colour = "#000000",
+			background_colour = "NotifyBackground",
+			fps = 60,
+			icons = {
+				DEBUG = "",
+				ERROR = "",
+				INFO = "",
+				TRACE = "✎",
+				WARN = "",
+			},
+			level = 2,
+			minimum_width = 30,
+			render = "compact",
+			stages = "fade",
+			time_formats = {
+				notification = "%T",
+				notification_history = "%FT%T",
+			},
+			timeout = 5000,
+			top_down = true,
 		})
 	end,
 }
